e340e2da955bf3e6d48898f490083d7d6e14c4cf
MatthewBertello <46825957+MatthewBertello@users.noreply.github.com>
2022-04-08 17:07:06 -0500

    Implement mathFunctions::map

--- mathFunctions.h
+++ mathFunctions.h
@@ -3,7 +3,10 @@
 
 namespace mathFunctions
 {
-
+    double map(double x, double minimumInput, double maximumInput, double minimumOutput, double maximumOutput)
+    {
+        return (x - minimumInput) * (maximumOutput - minimumOutput) / (maximumInput - minimumInput) + minimumOutput;
+    }
 }
 
 #endif
