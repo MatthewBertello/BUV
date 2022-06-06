341b445bc013088e51fc27e891ee962326195ea2
MatthewBertello <46825957+MatthewBertello@users.noreply.github.com>
2022-06-06 17:13:41 -0500

    Add comments for the map function

--- mathFunctions.h
+++ mathFunctions.h
@@ -3,6 +3,16 @@
 
 namespace mathFunctions
 {
+    /**
+     * Maps a value from one range to another.
+     *
+     * @param value The value to map
+     * @param minimumInput The low value of the range to map from
+     * @param maximumInput The high value of the range to map from
+     * @param minimumOutput The low value of the range to map to
+     * @param maximumOutput The high value of the range to map to
+     * @return The mapped value
+     */
     double map(double x, double minimumInput, double maximumInput, double minimumOutput, double maximumOutput)
     {
         return (x - minimumInput) * (maximumOutput - minimumOutput) / (maximumInput - minimumInput) + minimumOutput;
